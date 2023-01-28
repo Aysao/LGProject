b3cac94bacd03774a4d911906d23c2e34f4e7ef5
Aysao <matt.891999@hotmail.fr>
2023-01-28 20:26:33 +0100

    InitialisationProjet
    Initialisation d'un project maven

---
+++ lpgr/src/main/java/lp/loupgarou/App.java
@@ -0,0 +1,13 @@
+package lp.loupgarou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
