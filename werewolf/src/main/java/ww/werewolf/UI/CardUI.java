0525836fbc4fdb283cc53e238262fe76e8ee5b5f
Aysao <matt.891999@hotmail.fr>
2023-06-03 20:59:35 +0200

    Mise a jour création du rectangle et début de texture

---
+++ werewolf/src/main/java/ww/werewolf/UI/CardUI.java
@@ -0,0 +1,59 @@
+package ww.werewolf.UI;
+
+import org.lwjgl.opengl.GL11;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glVertex2f;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glEnd;
+
+public class CardUI {
+
+    private float x, y;
+    private String path = "../Assets/Card/loup.png";
+    // private String path = "./Assets/Card/loup.png";
+
+    // private Image;
+    private final int width = 250;
+    private int reswidth = 250;
+    private final int height = 400;
+    private int resheight = 400;
+    Texture t;
+
+    public CardUI() {
+        this.x = 0;
+        this.y = 0;
+        System.out.println("texture loading");
+        t = new Texture(reswidth, resheight, path);
+    }
+
+    public void DrawCard(int scalex, int scaley) {
+        CheckSize(scalex, scaley);
+        glEnable(GL_TEXTURE_2D);
+        System.out.println("lancement de la récupération de la texture ");
+
+        glBindTexture(GL_TEXTURE_2D, t.getTexID());
+
+        glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex2f(x, y);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex2f(x + reswidth, y);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex2f(x + reswidth, y + resheight);
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex2f(x, y + resheight);
+        glEnd();
+        glBindTexture(GL_TEXTURE_2D, 0);
+        // System.out.println(reswidth + " / " + resheight);
+
+    }
+
+    private void CheckSize(int scalex, int scaley) {
+        reswidth = (int) ((width * scalex) / 1920);
+        resheight = (int) ((height * scaley) / 1080);
+    }
+}
--- werewolf/src/main/java/ww/werewolf/UI/Window.java
+++ werewolf/src/main/java/ww/werewolf/UI/Window.java
@@ -1,18 +1,38 @@
 package ww.werewolf.UI;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glVertex2f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
 import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
 import static org.lwjgl.glfw.GLFW.GLFW_MAXIMIZED;
 import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
 import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
 import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
 import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
+import static org.lwjgl.glfw.GLFW.glfwGetFramebufferSize;
 import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
 import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
 import static org.lwjgl.glfw.GLFW.glfwShowWindow;
 import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
@@ -24,16 +44,26 @@ import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
-import org.lwjgl.Version;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import org.lwjgl.*;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
 public class Window {
 
+    private IntBuffer winw = BufferUtils.createIntBuffer(4);
+    private IntBuffer winh = BufferUtils.createIntBuffer(4);
+
     public static Window window = null;
     private int width, height;
     private String title;
     private long glfwWindow;
+    private CardUI c1;
 
     public static Window getWindow() {
         if (window == null)
@@ -46,7 +76,7 @@ public class Window {
         this.width = 1920;
         this.height = 1080;
         this.title = "Werewolf";
-
+        this.c1 = new CardUI();
     }
 
     public void run() {
@@ -78,23 +108,39 @@ public class Window {
             throw new IllegalStateException("Failed to create the GLFW window.");
         }
 
+        glfwSetKeyCallback(glfwWindow, (glfwWindow, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(glfwWindow, true); // We will detect this in the rendering loop
+        });
+
         glfwMakeContextCurrent(glfwWindow);
         // V-sync
         glfwSwapInterval(1);
 
         glfwShowWindow(glfwWindow);
-
         GL.createCapabilities();
 
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glEnable(GL_BLEND);
+        glOrtho(0, width, 0, height, -1, 1);
+        glMatrixMode(GL_MODELVIEW);
+        glDisable(GL_DEPTH_TEST);
     }
 
     public void loop() {
         while (!glfwWindowShouldClose(glfwWindow)) {
-            glfwPollEvents();
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwGetWindowSize(glfwWindow, winw, winh);
+            width = winw.get(0);
+            height = winh.get(0);
+
+            c1.DrawCard(width, height);
 
             glfwSwapBuffers(glfwWindow);
+            glfwPollEvents();
+            glClearColor(0.2f, 0.0f, 0.0f, 1.0f);
+
         }
         ;
     }
